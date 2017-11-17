4ba6a8f13b6c51143c0b47c23a8a09e677efb03e
Aditi Singh <aditisinghsikarwar@gmail.com>
2017-11-17 17:40:41 +0530

    Implemented  basic recipes list

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,6 +8,10 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
   encapsulation: ViewEncapsulation.None
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A testing Recipe', 'This is a recipe', 'http://images.media-allrecipes.com/userphotos/960x960/3758842.jpg'),
+    new Recipe('A testing Recipe 2', 'This is a recipe 2', 'http://images.media-allrecipes.com/userphotos/960x960/3758842.jpg')
+  ];
 
   constructor() { }
 
